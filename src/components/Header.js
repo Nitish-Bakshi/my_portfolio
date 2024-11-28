48e88c88e091496dabd193bc23bc8e6e5e4c140f
nitish <nirish@192.168.1.7>
2024-11-28 14:18:22 -0500

    navbar completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,43 +8,45 @@ import Logo from "../img/header/logo.png";
 
 const Header = () => {
   return (
-    <header className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
+    <header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
       <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
         {/*    logo   */}
         <Link to={"/"} className="max-w-[200px]">
           <img src={Logo} alt="" />
         </Link>
         {/* nav will be initially hidden and only will be visible in desktop mode  */}
-        <nav className="hidden xl:flex gap-x-12 font-semibold">
-          <Link
-            to={"/"}
-            className="text-[#696c6d] hover:text-primary transition"
-          >
-            Home
-          </Link>
-          <Link
-            to={"/about"}
-            className="text-[#696c6d] hover:text-primary transition"
-          >
-            About
-          </Link>
-          <Link
-            to={"/portfolio"}
-            className="text-[#696c6d] hover:text-primary transition"
-          >
-            Portfolio
-          </Link>
-          <Link
-            to={"/contact"}
-            className="text-[#696c6d] hover:text-primary transition"
-          >
-            Contact
-          </Link>
-        </nav>
-        {/* socials */}
-        <Socials />
-        {/* mobile nav */}
-        <MobileNav />
+        <div className="flex">
+          <nav className="hidden xl:flex gap-x-12 font-semibold">
+            <Link
+              to={"/"}
+              className="text-[#696c6d] hover:text-primary transition"
+            >
+              Home
+            </Link>
+            <Link
+              to={"/about"}
+              className="text-[#696c6d] hover:text-primary transition"
+            >
+              About
+            </Link>
+            <Link
+              to={"/portfolio"}
+              className="text-[#696c6d] hover:text-primary transition"
+            >
+              Portfolio
+            </Link>
+            <Link
+              to={"/contact"}
+              className="text-[#696c6d] hover:text-primary transition"
+            >
+              Contact
+            </Link>
+          </nav>
+          {/* socials */}
+          <Socials />
+          {/* mobile nav */}
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,42 @@
 import React from "react";
+// import icons
+import { ImTwitter, ImInstagram, ImLinkedin, ImGithub } from "react-icons/im";
 
 const Socials = () => {
-  return <div className="hidden xl:flex ml-24">Socials</div>;
+  return (
+    <div className="hidden xl:flex ml-24">
+      <ul className="flex gap-x-4 items-center">
+        <li>
+          <a href="https://www.twitter.com/nitishbakshi001" target="_blank">
+            <ImTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/nitishbakshi001" target="_blank">
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/nitish-bakshi-b2368a271
+"
+            target="_blank"
+          >
+            <ImLinkedin />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.github.com/Nitish-Bakshi
+"
+            target="_blank"
+          >
+            <ImGithub />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
