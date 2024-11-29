8d50d9b4e2840652f2c0b866ac18f7312c0d4eed
nitish <nirish@192.168.1.7>
2024-11-29 10:34:59 -0500

    edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+
 // import images
+
 // import links
+
 import meImg from "../img/home/me.png";
+
 // import Link
+
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -10,24 +15,30 @@ const Home = () => {
     <section className="section ">
       <div className="container mx-auto h-full relative">
         {/* text & image wrapper*/}
+
         <div className="flex flex-col justify-center ">
           {/* text */}
+
           <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
             <h1 className="h1">
               web developer
               <br />& Designer
             </h1>
+
             <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
               India
             </p>
+
             <Link to={"/contact"} className="btn mb-[30px] ">
               hire me
             </Link>
           </div>
+
           {/* image */}
+
           <div className="flex justify-end max-h-96 lg:max-h-max">
             <div className="relative lg:-right-40 overflow-hidden">
-              <img className="w-[390] h-[531]" src={meImg} alt="woman" />
+              <img src={meImg} alt="man" />
             </div>
           </div>
         </div>
