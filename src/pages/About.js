9584ee79977bb06c3957d16b31a6fa4604289850
nitish <nirish@192.168.1.7>
2024-11-29 14:03:46 -0500

    aboutme

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,40 @@
 import React from "react";
+// IMPORT IMAGES
+import meImg from "../img/home/me.png";
+// import links
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return <section className="section bg-yellow-200">About</section>;
+  return (
+    <section className="section ">
+      <div className="container mx-auto h-full relative">
+        {/* text & image wrapper*/}
+        <div className=" flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
+          {/* image */}
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <img src={meImg} alt="" />
+          </div>
+          {/* text */}
+          <div className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
+            <h1 className="h1">About me</h1>
+            <p className="mb-12 max-w-sm">
+              I'm <b>Nitish Bakshi</b>, a web developer and designer from India,
+              always excited to dive into the world of pixels and code. My
+              journey into the digital realm is fueled by a passion for crafting
+              beautiful and functional websites.
+              <br />
+              <br />I love turning ideas into reality, transforming complex
+              concepts into elegant digital experiences. Let's create something
+              amazing together!
+            </p>
+            <Link className="btn" to={"/portfolio"}>
+              View My Work
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,168 @@
 import React from "react";
+// import motions
+import { motion } from "motion/react";
+// import links
+import { Link } from "react-router-dom";
 
+const angular = (delay) => ({
+  leftupcorner: {
+    opacity: 0,
+    x: -100,
+    y: -100,
+  },
+  rightdowncorner: {
+    opacity: 0,
+    x: 100,
+    y: 100,
+  },
+  show1: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      delay: delay,
+    },
+  },
+  show2: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 0.7,
+      delay: delay,
+    },
+  },
+});
+const horizontal = (delay) => ({
+  leftdowncorner: {
+    opacity: 0,
+    x: -100,
+    y: 100,
+  },
+  rightupcorner: {
+    opacity: 0,
+    x: 100,
+    y: -100,
+  },
+  show3: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: delay,
+    },
+  },
+  show4: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      delay: delay,
+    },
+  },
+});
+const vertical = (delay) => ({
+  centerup: {
+    opacity: 0,
+    y: 100,
+    x: 0,
+    transition: {
+      duration: 0.6,
+      delay: delay,
+    },
+  },
+  centerdown: {
+    opacity: 0,
+    y: -100,
+    x: 0,
+    transition: {
+      duration: 0.8,
+      delay: delay,
+    },
+  },
+  show5: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      delay: delay,
+    },
+  },
+  show6: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 0.7,
+      delay: delay,
+    },
+  },
+});
 const Portfolio = () => {
-  return <section className="section bg-green-200">Portfolio</section>;
+  return (
+    <section className="section ">
+      <div className="container mx-auto h-full relative flex flex-col justify-center items-center">
+        <div className="my-4 sm:my-6 md:my-8 lg:my-12 xl:my-14">
+          <h1 className="h1 ">My Portfolio</h1>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 w-full h-[60%]">
+          <motion.div
+            variants={angular(0.2)}
+            initial="leftupcorner"
+            whileInView="show1"
+            className="block bg-blue-500"
+          >
+            Item 1
+          </motion.div>
+          <motion.div
+            variants={vertical(0.4)}
+            initial="centerup"
+            whileInView="show5"
+            className="block bg-green-500"
+          >
+            Item 2
+          </motion.div>
+          <motion.div
+            variants={horizontal(0.6)}
+            initial="rightupcorner"
+            whileInView="show4"
+            className="block bg-red-500"
+          >
+            Item 3
+          </motion.div>
+          <motion.div
+            variants={horizontal(0.8)}
+            initial="leftdowncorner"
+            whileInView="show3"
+            className="block bg-yellow-500"
+          >
+            Item 4
+          </motion.div>
+          <motion.div
+            variants={vertical(1)}
+            initial="centerup"
+            whileInView="show6"
+            className="block bg-purple-500"
+          >
+            Item 5
+          </motion.div>
+          <motion.div
+            variants={angular(1.2)}
+            initial="rightdowncorner"
+            whileInView="show2"
+            className="block bg-pink-500"
+          >
+            Item 6
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
